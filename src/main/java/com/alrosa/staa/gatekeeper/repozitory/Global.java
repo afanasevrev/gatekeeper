52cac46817ed2b7ee9f93b675d177f4580678c30
unknown <rewoly@mail.ru>
2023-11-13 16:55:00 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/repozitory/Global.java
+++ src/main/java/com/alrosa/staa/gatekeeper/repozitory/Global.java
@@ -1,4 +1,14 @@
 package com.alrosa.staa.gatekeeper.repozitory;
 
+import com.alrosa.staa.gatekeeper.Direction;
+
+import java.util.UUID;
+
 public abstract class Global {
+    //Метод для возврата имени класса
+    public abstract String getCompleteName();
+    //Метод для возврата уникального унифицированного id, который хранится в базе данных проекта
+    public abstract UUID getId();
+    //Метод для возврата enum-a
+    public abstract Direction getDirection();
 }
