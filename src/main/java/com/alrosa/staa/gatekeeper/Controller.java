920c06655c6bdff85cf82f7d20b1972944a6d7e6
unknown <rewoly@mail.ru>
2023-11-13 12:15:57 +0900

    work on the admins console

---
+++ src/main/java/com/alrosa/staa/gatekeeper/Controller.java
@@ -0,0 +1,8 @@
+package com.alrosa.staa.gatekeeper;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+}
--- src/main/java/com/alrosa/staa/gatekeeper/StartGateKeeper.java
+++ src/main/java/com/alrosa/staa/gatekeeper/StartGateKeeper.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StartGateKeeper extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGateKeeper.class.getResource("gatekeeper.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        stage.setTitle("Gate Keeper Secure Access");
         stage.setScene(scene);
         stage.show();
     }
