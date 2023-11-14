71fdbd8ff96939bf0a0be7eca4b52f061afa672b
unknown <rewoly@mail.ru>
2023-11-14 14:28:42 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/StartGateKeeper.java
+++ src/main/java/com/alrosa/staa/gatekeeper/StartGateKeeper.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
 public class StartGateKeeper extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -16,7 +14,6 @@ public class StartGateKeeper extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
