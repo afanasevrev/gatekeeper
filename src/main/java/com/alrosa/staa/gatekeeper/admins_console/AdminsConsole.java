722fcbd9f732461ad0a8258cd0b3e58a80b12789
afanasevrev <rewoly@mail.ru>
2023-11-15 23:42:37 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
+++ src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
@@ -9,15 +9,15 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AdminsConsole extends Application {
-    private Image image = new Image(StartGateKeeper.class.getResource("favicon/knight.png").toString());
+    private Image image = new Image(StartGateKeeper.class.getResource("favicon/shield.png").toString());
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
         //Parent root = FXMLLoader.load(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),450, 450);
+        Scene scene = new Scene(fxmlLoader.load(),700, 550);
         //Scene scene = new Scene(root);
         stage.setMaximized(true);
-        stage.setTitle("Gatekeeper - guard of security");
+        stage.setTitle("Gatekeeper - Guard of Security");
         stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
