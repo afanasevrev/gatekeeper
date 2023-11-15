69c2c5a40a275cb1d7408944a485dc960e5e22a8
unknown <rewoly@mail.ru>
2023-11-15 17:23:53 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
+++ src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
@@ -5,17 +5,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AdminsConsole extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        //FXMLLoader fxmlLoader = new FXMLLoader(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
-        Parent root = FXMLLoader.load(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(),450, 450);
-        Scene scene = new Scene(root);
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
+        //Parent root = FXMLLoader.load(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),450, 450);
+        //Scene scene = new Scene(root);
         stage.setMaximized(true);
         stage.setTitle("Gatekeeper - guard of security");
+        stage.getIcons().add(new Image("favicon/knight"));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.alrosa.staa.gatekeeper {
     requires javafx.fxml;
     requires javafx.graphics;
 
+    opens com.alrosa.staa.gatekeeper.favicon;
     opens com.alrosa.staa.gatekeeper.admins_console;
     opens com.alrosa.staa.gatekeeper to javafx.fxml;
     exports com.alrosa.staa.gatekeeper;
