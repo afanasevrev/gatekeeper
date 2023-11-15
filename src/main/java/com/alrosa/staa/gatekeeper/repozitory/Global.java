5d27454ac9427225028f903cff0cbcf1a566d98c
afanasevrev <rewoly@mail.ru>
2023-11-16 00:48:41 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/repozitory/Global.java
+++ src/main/java/com/alrosa/staa/gatekeeper/repozitory/Global.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 public abstract class Global {
     //Метод для возврата имени класса
     public abstract String getCompleteName();
+    //Метод для замены имени класса
+    public abstract void setCompleteName(String completeName);
     //Метод для возврата уникального унифицированного id, который хранится в базе данных проекта
     public abstract UUID getId();
     //Метод для возврата Enum-a
