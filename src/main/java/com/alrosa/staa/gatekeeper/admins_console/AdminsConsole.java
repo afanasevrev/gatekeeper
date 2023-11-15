24906bc5189feb1214fa11ca2f241aab26e6d0ce
unknown <rewoly@mail.ru>
2023-11-15 14:44:08 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
+++ src/main/java/com/alrosa/staa/gatekeeper/admins_console/AdminsConsole.java
@@ -3,16 +3,13 @@ package com.alrosa.staa.gatekeeper.admins_console;
 import com.alrosa.staa.gatekeeper.StartGateKeeper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AdminsConsole extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-       // Parent root = new FXMLLoader().load(getClass().getResource("admins_console/admins_console.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(StartGateKeeper.class.getResource("admins_console/admins_console.fxml"));
-       // Scene scene = new Scene(root);
         Scene scene = new Scene(fxmlLoader.load(),450, 450);
         stage.setTitle("Gatekeeper - guard of security");
         stage.setScene(scene);
