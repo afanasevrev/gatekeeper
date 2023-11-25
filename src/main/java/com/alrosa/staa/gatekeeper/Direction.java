4f7f2e8f0461dea33259e0d0051cf7281a67234b
afanasevrev <rewoly@mail.ru>
2023-11-25 23:18:50 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/Direction.java
+++ src/main/java/com/alrosa/staa/gatekeeper/Direction.java
@@ -1,11 +1,45 @@
 package com.alrosa.staa.gatekeeper;
 
 public enum Direction {
+    //Главный объект
     MAIN,
-    BUREAU, USERS, ADMINS, OPERATORS, GLOBAL_ACCESS_LEVELS, CARDS, CARD_LAYOUTS, POSITIONS, ORGANIZATIONS,
-    MAN, WOMAN, GLOBAL_ACCESS_LEVEL, CARD, CARD_LAYOUT, POSITION, ORGANIZATION, OFFICE,
-    COMPUTER, CONSOLE,
-    SERVER, PERCO,  PERCOC01, CARDREADER;
 
+    //Объекты MAIN
+    BUREAU, COMPUTER, SERVER,
 
+    //Объекты BUREAU
+    USERS, ADMINS, OPERATORS, GLOBAL_ACCESS_LEVELS, CARDS, CARD_LAYOUTS, POSITIONS, ORGANIZATIONS,
+
+    //Объекты SERVER
+    PERCO,
+
+    //Объекты PERCO
+    PERCOC01,
+
+    //Объекты PERCOC01
+    CARDREADER,
+
+    //Объекты COMPUTER
+    CONSOLE,
+
+    //Объекты USERS, ADMINS, OPERATORS
+    MAN, WOMAN,
+
+    //Объекты GLOBAL_ACCESS_LEVELS
+    GLOBAL_ACCESS_LEVEL,
+
+    //Объекты CARDS
+    CARD,
+
+    //Объекты CARD_LAYOUTS
+    CARD_LAYOUT,
+
+    //Объекты POSITIONS
+    POSITION,
+
+    //Объекты ORGANIZATIONS
+    ORGANIZATION,
+
+    //Объекты ORGANIZATION
+    OFFICE;
 }
