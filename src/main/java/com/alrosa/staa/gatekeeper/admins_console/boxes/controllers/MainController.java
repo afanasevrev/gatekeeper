fb97f77592f9d6042ef01184ddc55e960e975fc2
unknown <rewoly@mail.ru>
2023-12-06 10:44:23 +0900

    work on the admins console

---
+++ src/main/java/com/alrosa/staa/gatekeeper/admins_console/boxes/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.alrosa.staa.gatekeeper.admins_console.boxes.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable  {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
