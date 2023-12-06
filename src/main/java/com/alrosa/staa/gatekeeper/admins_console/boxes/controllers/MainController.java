8443f437b66219fb86eb2b9e6e71da537bb0023f
unknown <rewoly@mail.ru>
2023-12-06 12:22:54 +0900

    work on the admins console

--- src/main/java/com/alrosa/staa/gatekeeper/admins_console/boxes/controllers/MainController.java
+++ src/main/java/com/alrosa/staa/gatekeeper/admins_console/boxes/controllers/MainController.java
@@ -1,14 +1,19 @@
 package com.alrosa.staa.gatekeeper.admins_console.boxes.controllers;
 
+import com.alrosa.staa.gatekeeper.Variables;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    private String text;
+    @FXML
+    TextField textField = new TextField();
     @FXML
     private AnchorPane mainPane = new AnchorPane();
     @FXML
@@ -18,6 +23,9 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        text = Variables.adminsConsoleItem.getValue().getCompleteName();
+        textField.setText(text);
+
         AnchorPane.setRightAnchor(mainPane, 0.0);
         AnchorPane.setLeftAnchor(mainPane, 0.0);
         AnchorPane.setTopAnchor(mainPane, 0.0);
